c3aabed70f8277f9d143037b33c53f398acd8bfa
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-10-21 23:56:52 +0900

    htmlクラス等修正

--- index.php
+++ index.php
@@ -20,15 +20,15 @@
         <?php endfor; ?>
       </div>
 
-      <main class="stage <?php echo (empty($_SESSION['knockDownCount'])) ?'stage--before' :'stage--after'; ?>">
+      <main class="stage <?php echo (!isset($_SESSION['knockDownCount'])) ?'stage--before' :'stage--after'; ?>">
         <div class="stage-wrapper">
 
-        <?php if (empty($_SESSION['knockDownCount'])): ?>
+        <?php if(!$_SESSION): ?>
           <h2 class="stage_title">〜HumanBeatBoxのバトルへようこそ〜</h2>
           <div class="stage_contents contents--center">
             <div class="field">
               <p class="field_introText">
-                名だたるビートボクサーが日本に集結！<br>
+                世界のビートボクサーが日本に集結！<br>
                 チームJapan VS チームWorld<br>
                 ビートボックスバトルが幕を開ける！
               </p>
@@ -38,34 +38,50 @@
             </div>
           </div>
 
-        <?php elseif (empty($_SESSION['knockDownCount']) && $_SESSION['knockDownCount'] < 7) : ?>
-        <h2 class="stage_title"><?php// echo .'VS'. ?></h2>
-        <p>現在の連勝記録：<?php //echo knockDownCount ?></p>
-        <div class="stage_contents contents--between">
-          <div class="field field--player">
-            <img src="#" class="field_img" alt="">
-            <p>自分：<?php // echo 名前 ?></p>
-            <p>残りのHP：<?php //自分のHP ?></p>
-            <form class="field_action" method="post">
-              <input type="submit" class="btn btn--center" name="attack" value="演奏する">
-              <input type="submit" class="btn btn--center" name="healing" value="水を飲む">
-              <input type="submit" class="btn btn--center" name="" value="逃げる">
-              <input type="submit" class="btn btn--center" name="start" value="リスタート">
-            </form>
+        <?php elseif ($_SESSION['knockDownCount'] < 7) : ?>
+          <h2 class="stage_title">
+            <span style="width:300px; text-align:right;"><?php echo $_SESSION['japan']->getName(); ?></span>
+            <span style="width:100px;">vs</span>
+            <span style="width:300px; text-align:left;"><?php echo $_SESSION['world']->getName(); ?></span>
+          </h2>
+          <p class="stage_knockDownCount">現在の連勝記録：<?php echo $_SESSION['knockDownCount']; ?></p>
+          <div class="stage_contents contents--between">
+            <div class="field field--player">
+              <img src="#" class="field_img" alt="">
+              <p>自分：<?php echo $_SESSION['japan']->getName(); ?></p>
+              <p>残りのHP：<?php echo $_SESSION['japan']->getHp(); ; ?></p>
+              <form class="field_action" method="post">
+                <input type="submit" class="btn btn--center" name="attack" value="演奏する">
+                <input type="submit" class="btn btn--center" name="healing" value="水を飲む">
+                <input type="submit" class="btn btn--center" name="escape" value="逃げる">
+                <input type="submit" class="btn btn--center" name="start" value="はじめから">
+              </form>
+            </div>
+
+            <div class="judge">
+              <?php echo $_SESSION['judge']; ?>
+            </div>
+
+            <div class="field field--player">
+              <img src="#" class="field_img" alt="">
+              <p>相手：<?php echo $_SESSION['world']->getName() ; ?></p>
+              <p>残りのHP：<?php echo $_SESSION['world']->getHp(); ?></p>
+
+            </div>
           </div>
 
-          <div class="judge">
-            <?php // echo judge; ?>
+        <?php else: ?>
+          <h2 class="stage_title">〜congratulations〜</h2>
+          <div class="stage_contents contents--center">
+            <div class="field">
+              <p class="field_introText">
+                名だたるビートボクサーに勝利した！<br>
+              </p>
+              <form class="field_action" method="post">
+                <input type="submit" class="btn btn--center" name="reset" value="トップにもどる">
+              </form>
+            </div>
           </div>
-
-          <div class="field field--player">
-            <img src="#" class="field_img" alt="">
-            <p>相手：<?php // echo 名前 ?></p>
-            <p>残りのHP：<?php //自分のHP ?></p>
-
-          </div>
-        </div>
-
 
         <?php endif; ?>
         </div>
--- object.php
+++ object.php
@@ -10,6 +10,7 @@ $beatBoxersW = array();
 abstract class Human {
   // プロパティ
   protected $name;
+  protected $img;
   protected $hp;
   protected $minAttack;
   protected $maxAttack;
@@ -18,7 +19,7 @@ abstract class Human {
     $this -> name = $name;
     $this -> img = $img;
     $this -> hp = $hp;
-    $this -> minAtaack = $minAttack;
+    $this -> minAttack = $minAttack;
     $this -> maxAttack = $maxAttack;
   }
   // セッター・ゲッター
@@ -40,28 +41,37 @@ abstract class Human {
 
   // 攻撃メソッド
   public function attack($opponent){
-    Judge::set($this->getName().'のビート');
-    $attackPoint = mt_rand($this->minAttack(), $this->maxAttack());
+    Judge::set($this->getName().'の攻撃');
+    $attackPoint = mt_rand($this->minAttack, $this->maxAttack);
     // 4分の1の確率で必殺ビート
     if (!mt_rand(0, 3)) {
       Judge::set('必殺ビートが飛び出した！！');
       $attackPoint *= 1.5;
     }
-    Judge::set($attackPoint.'ポイントの判定');
+    Judge::set($attackPoint.'ポイントのダメージ');
     $opponent -> setHp($opponent->getHp() - $attackPoint);
   }
 }
 
 // 日本人クラス
 class Japan extends Human{
+  protected $experience;
   protected $minHealing;
   protected $maxHealing;
 
-  public function __construct($name, $img, $hp, $minAttack, $maxAttack, $minHealing, $maxHealing){
+  public function __construct($name, $img, $hp, $minAttack, $maxAttack, $experience, $minHealing, $maxHealing){
     parent::__construct($name, $img, $hp, $minAttack, $maxAttack);
+    $this -> experience = $experience;
     $this -> minHealing = $minHealing;
     $this -> maxHealing = $maxHealing;
   }
+  public function setExperience($num){
+    $this -> experience = $num;
+  }
+  public function getExperience(){
+    return $this -> experience;
+  }
+
   // 回復メソッド
   public function healing($healer){
     Judge::set($healer->getName().'は水を飲んだ');
@@ -71,27 +81,38 @@ class Japan extends Human{
       Judge::set('のどの調子がかなり良くなった！！');
       $healPoint *= 2;
     }
-    Judge::set($healPoint.'ポイント分の体力を回復した');
+    Judge::set($healPoint.'ポイント回復');
     $healer->setHp($healer->getHp() + $healPoint);
   }
 }
 
 // 外国人クラス
-class World extends Human{}
+// 倒した場合の経験値を追加
+class World extends Human{
+  protected $level;
+  public function __construct($name, $img, $hp, $minAttack, $maxAttack, $level){
+    parent::__construct($name, $img, $hp, $minAttack, $maxAttack);
+    $this -> level = $level;
+  }
+  public function getLevel(){
+    return $this -> level;
+  }
+}
+
 // レジェンドクラス
 class Legend extends World{
   protected $legendBeat;
-  public function __construct($name, $img, $hp, $minAttack, $maxAttack, $legendBeat){
-    parent::__construct($name, $img, $hp, $minAttack, $maxAttack);
+  public function __construct($name, $img, $hp, $minAttack, $maxAttack, $level, $legendBeat){
+    parent::__construct($name, $img, $hp, $minAttack, $maxAttack, $level);
     $this -> legendBeat = $legendBeat;
   }
   // 攻撃クラスオーバーライド
   public function attack($opponent){
     // 5分の1の確率で伝説のビート
-    if (!empty(mt_rand(0, 4))) {
+    if (empty(mt_rand(0, 4))) {
       Judge::set($this->getName().'の伝説のビートだ！！！！！');
       $attackPoint = $this -> legendBeat;
-      Judge::set($attackPoint.'ポイントの判定');
+      Judge::set($attackPoint.'ポイントのダメージ');
       $opponent -> setHp($opponent->getHp() - $attackPoint);
     }else {
       parent::attack($opponent);
@@ -99,8 +120,14 @@ class Legend extends World{
   }
 }
 
+interface InterfaceJudge {
+  static function set($str);
+  static function clear();
+}
+
+
 // 進行役クラス
-class Judge {
+class Judge implements InterfaceJudge {
   static function set($str){
     if (empty($_SESSION['judge'])) {
       $_SESSION['judge'] = '';
@@ -113,28 +140,28 @@ class Judge {
   }
 }
 
-// 日本インスタンス $beatBoxersJ[] = new Japan($name, $img, $hp, $minAttack, $maxAttack, $minHealing, $maxHealing);
-$beatBoxersJ[] = new Japan('SHOW-GO', '/img/japan.png', 400, 120, 200, 100, 150);
-$beatBoxersJ[] = new Japan('HIKAKIN', '/img/japan.png', 700, 50, 80, 20, 50);
-$beatBoxersJ[] = new Japan('so-so', '/img/japan.png', 500, 80, 130, 50, 80);
-$beatBoxersJ[] = new Japan('Rofu', '/img/japan.png', 800, 100, 150, 20, 50);
-$beatBoxersJ[] = new Japan('SARUKANI', '/img/japan.png', 1000, 80, 100, 150, 200);
-$beatBoxersJ[] = new Japan('4th GAS', '/img/japan.png', 1000, 130, 230, 50, 100);
-// 世界インスタンス $beatBoxersJ[] = new World($name, $img, $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('Gene Shinozaki', '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World("RIVER'", '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('Robin', '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('Rhythmind', '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('Middle School', '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('Rogue Wave', '/img/world.png', $hp, $minAttack, $maxAttack);
-$beatBoxersW[] = new World('The Beatbox House', '/img/world.png', $hp, $minAttack, $maxAttack);
+// 日本インスタンス $beatBoxersJ[] = new Japan($name, $img, $hp, $minAttack, $maxAttack, $experience, $minHealing, $maxHealing);
+$beatBoxersJ[] = new Japan('SHOW-GO', '/img/japan.png', 400, 120, 200, 1, 100, 150);
+$beatBoxersJ[] = new Japan('HIKAKIN', '/img/japan.png', 700, 50, 80, 20, 1, 50);
+$beatBoxersJ[] = new Japan('so-so', '/img/japan.png', 500, 80, 130, 50, 1, 80);
+$beatBoxersJ[] = new Japan('Rofu', '/img/japan.png', 800, 100, 150, 20, 1, 50);
+$beatBoxersJ[] = new Japan('SARUKANI', '/img/japan.png', 1000, 80, 100, 1, 150, 200);
+$beatBoxersJ[] = new Japan('4th GAS', '/img/japan.png', 1000, 130, 230, 1, 50, 100);
+// 世界インスタンス $beatBoxersJ[] = new World($name, $img, $hp, $minAttack, $maxAttack, $level);
+$beatBoxersW[] = new World('Gene Shinozaki', '/img/world.png', 200, 30, 50, 1);
+$beatBoxersW[] = new World("RIVER'", '/img/world.png', 150, 30, 50, 1);
+$beatBoxersW[] = new World('Robin', '/img/world.png', 100, 30, 50, 1);
+$beatBoxersW[] = new World('Rhythmind', '/img/world.png', 180, 40, 60, 1);
+$beatBoxersW[] = new World('Middle School', '/img/world.png', 210, 30, 60, 1);
+$beatBoxersW[] = new World('Rogue Wave', '/img/world.png', 200, 30, 70, 1);
+$beatBoxersW[] = new World('TheBeatboxHouse', '/img/world.png', 250, 40, 60, 1);
 
-// レジェンドインスタンス  　$beatBoxersW[] = new Legend($name, $img, $hp, $minAttack, $maxAttack, $legendBeat);
-$beatBoxersW[] = new Legend('D-low', '/img/world.png', $hp, $minAttack, $maxAttack, $legendBeat);
-$beatBoxersW[] = new Legend('CodFish', '/img/world.png', $hp, $minAttack, $maxAttack, $legendBeat);
-$beatBoxersW[] = new Legend('SARO', '/img/world.png', $hp, $minAttack, $maxAttack, $legendBeat);
-$beatBoxersW[] = new Legend('SPIDER HORCE', '/img/world.png', $hp, $minAttack, $maxAttack, $legendBeat);
-$beatBoxersW[] = new Legend('Berywam', '/img/world.png', $hp, $minAttack, $maxAttack, $legendBeat);
+// レジェンドインスタンス  　$beatBoxersW[] = new Legend($name, $img, $hp, $minAttack, $maxAttack, $level, $legendBeat);
+$beatBoxersW[] = new Legend('D-low', '/img/world.png', 350, 80, 100, 2, 150);
+$beatBoxersW[] = new Legend('CodFish', '/img/world.png', 300, 60, 80, 2, 130);
+$beatBoxersW[] = new Legend('SARO', '/img/world.png', 350, 60, 90, 2, 130);
+$beatBoxersW[] = new Legend('SPIDER HORCE', '/img/world.png', 400, 70, 120, 2, 150);
+$beatBoxersW[] = new Legend('Berywam', '/img/world.png', 420, 50, 120, 2, 150);
 
 
 
@@ -152,7 +179,7 @@ function createWorld(){
   Judge::set('相手は'.$representW->getName().'だ');
   $_SESSION['world'] = $representW;
 }
-function restart(){
+function start(){
   Judge::clear();
   $_SESSION['knockDownCount'] = 0;
   Judge::set('3, 2, 1, ビートボックス！！');
@@ -167,32 +194,40 @@ function gameOver(){
 // POST送信で処理スタート
 
 if (!empty($_POST)) {
+
   $startFlg = (!empty($_POST['start'])) ? true : false ;
   $attackFlg = (!empty($_POST['attack'])) ? true : false ;
   $healingFlg = (!empty($_POST['healing'])) ? true : false ;
   $escapeFlg = (!empty($_POST['escape'])) ? true : false ;
+  $resetFlg = (!empty($_POST['reset'])) ? true : false;
 
   // スタートおよびリスタート
   if ($startFlg) {
-    restart();
+    start();
   }
 
   // 攻撃もしくは回復
   if ($attackFlg || $healingFlg) {
+    // 攻撃する場合
     if ($attackFlg) {
-      // こちらのターン、相手が倒れたら次の相手
       $_SESSION['japan'] -> attack($_SESSION['world']);
-
+      // 敵のHPが0以下になったら次に進む
       if ($_SESSION['world']->getHp() <= 0) {
         Judge::set($_SESSION['japan']->getName().'の勝利！');
         $_SESSION['knockDownCount'] += 1;
         createWorld();
+        return;
+
       }
+    // 回復する場合
     }elseif ($healingFlg) {
+      // Japanの回復ターン
       $_SESSION['japan'] -> healing($_SESSION['japan']);
     }
-    // その後相手のターン、自分が倒れたらゲームオーバー
+
+    // その後Worldのターン、JapanのHPが0になったらゲームオーバー
     $_SESSION['world'] -> attack($_SESSION['japan']);
+
     if ($_SESSION['japan']->getHp() <= 0) {
       gameOver();
     }
@@ -202,6 +237,11 @@ if (!empty($_POST)) {
   if ($escapeFlg) {
     Judge::set('棄権した');
     createWorld();
+  }
+
+  // リセット
+  if ($resetFlg) {
+    session_destroy();
   }
 }
 
